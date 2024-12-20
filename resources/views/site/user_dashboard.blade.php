951053156bc9652697282cc5df857a81ad55c0ba
Snehasish Bhurisrestha <codofficial2023snehasish@gmail.com>
2024-12-20 18:10:01 +0530

    20-12-2024

--- resources/views/site/user_dashboard.blade.php
+++ resources/views/site/user_dashboard.blade.php
@@ -241,7 +241,7 @@
                                 <div class="accordion-item">
                                     <h2 class="accordion-header" id="flush-headingOne">
                                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                            Order Date : {!! formated_date($order->created_at) !!} , Order number : {{ $order->order_number }}
+                                            Order Date : {!! format_datetime($order->created_at) !!} , Order number : {{ $order->order_number }}
                                         </button>
                                     </h2>
                                     <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
